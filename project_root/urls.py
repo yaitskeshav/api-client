b293b1d88d216beb6948e8628841112b91dd6444
keshav <keshavjha1504@gmail.com>
2022-05-06 22:48:44 +0530

    Update urls.py

--- project_root/urls.py
+++ project_root/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/keshav', admin.site.urls),
     path('', include('app1.urls')),
 ]
