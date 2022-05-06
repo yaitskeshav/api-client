f82ea469c72d0adf38b5c668f1f7844a433635d8
keshav <keshavjha1504@gmail.com>
2022-05-06 22:52:21 +0530

    some more changes for static files

--- project_root/settings.py
+++ project_root/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +134,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     
 ]
+django_heroku.settings(locals())
