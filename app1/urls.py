506f7b4bbbaf47f95425d42e15fbf393f20506e2
keshav <keshavjha1504@gmail.com>
2022-05-18 19:53:56 +0530

    Changed some API access method

--- app1/urls.py
+++ app1/urls.py
@@ -7,7 +7,12 @@ urlpatterns = [
     path('BornesHorsRue', views.BornesHorsRue),
     # path('fd', views.fetchdata),
     path('fd/<slug:db>/<slug:password>', views.fetchdata),
-    path('fd/', views.fetchdata),
-    path('fd', views.fetchdata),
-    path('fd/<slug:db>', views.fetchdata),
+    
+    path('fd/<slug:db>/<slug:password>/', views.fetchdata),
+
+    path('fd/<slug:db>/<slug:password>/<slug:count>', views.fetchdata),
+    path('fd/<slug:db>/<slug:password>/<slug:initial>/<slug:final>', views.fetchdata),
+    # path('fd/', views.fetchdata),
+    # path('fd', views.fetchdata),
+    # path('fd/<slug:db>', views.fetchdata),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,6 +1,6 @@
 
 import json
-from django.http import HttpResponse, JsonResponse
+from django.http import Http404, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 from app1.models import *
@@ -19,34 +19,69 @@ def BornesHorsRue(request):
         print("printing type of data ////" , type(data))
         return JsonResponse(data, safe=False)
     return HttpResponse("NOT A REQUEST")
-def fetchdata(request,password="none",db="BH",):
-    if password=="keshavjha":
-        if   db =="BH":
-            db=BornesHorsRueDB
-        elif db=="BS":
-            db=BornesSurRueDB
-        elif db=="DE":
-            db=DateExportationDB
-        elif db=="ER":
-            db=EmplacementReglementationBD
-        elif db=="PR":
-            db=PeriodesDB
-        elif db=="PC":
-            db=PlacesDB
-        elif db=="RP":
-            db=ReglementationPeriodeDB
-        elif db=="RL":
-            db=ReglementationsDB
+
+
+def fetchdata(request,password="none",db="BH",count=None,initial=None,final=None):
+    print(count)
+    try:
+
+        if password=="keshavjha":
+            if  db =="BH":
+                db=BornesHorsRueDB
+            elif db=="BS":
+                db=BornesSurRueDB
+            elif db=="DE":
+                db=DateExportationDB
+            elif db=="ER":
+                db=EmplacementReglementationBD
+            elif db=="PR":
+                db=PeriodesDB
+            elif db=="PC":
+                db=PlacesDB
+            elif db=="RP":
+                db=ReglementationPeriodeDB
+            elif db=="RL":
+                db=ReglementationsDB
+
+            else:
+                db=BornesHorsRueDB
+            
+            # userobject=database.objects.all()
+            if ((count==None) and (initial==None) and (final==None)):
+                data=list(db.objects.values())
+
+
+
+            elif count:
+                try:
+                    data=list(db.objects.values()[:int(count)])
+                except:
+                    
+                    return HttpResponse("No Data Found ;)")
+
+
+            elif ( initial == None) and (final == None ):
+                data=list(db.objects.values())
+                
+
+            elif (initial and final):
+                try:
+                    data=list(db.objects.values()[int(initial)-1:int(final)])
+                except:
+                    return HttpResponse("No Data Found")
+
+            # serialisedObject=BornesHorsRueAPI(userobject,many=True)
+
+            # jsonFormat=json.dumps(serialisedObject.data)
+            # return JsonResponse(serialisedObject.data,safe=False)
+            # return JsonResponse(json.loads(jsonFormat),safe=False)
+            if len(data)==0:
+                return HttpResponse("No Data Found")
+            else:    
+                return JsonResponse(data, safe=False)
+
         else:
-            db=BornesHorsRueDB
-        # userobject=database.objects.all()
-        data=list(db.objects.values())
-        # serialisedObject=BornesHorsRueAPI(userobject,many=True)
-
-        # jsonFormat=json.dumps(serialisedObject.data)
-        # return JsonResponse(serialisedObject.data,safe=False)
-        # return JsonResponse(json.loads(jsonFormat),safe=False)
-
-        return JsonResponse(data, safe=False)
-    else:
-        return HttpResponse("You are not authorised")
+            return HttpResponse("You are not authorised")
+    
+    except:
+        return HttpResponse("Something Went Wrong ;) Try Contacting Keshav")
